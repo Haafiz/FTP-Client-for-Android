b6192c31be9f2b8e56717784bb9cc2ed3cd44177
Haafiz <kaasib@gmail.com>
2015-09-13 01:51:46 +0500

    adding connection log box

---
+++ src/main/java/com/example/hafiz/ftp/FTPTask.java
@@ -0,0 +1,21 @@
+/*package com.example.hafiz.ftp;
+
+import android.util.Log;
+
+/**
+ * Created by Hafiz on 9/13/2015.
+ *//*
+class FtpTask extends AsyncTask<Void, Void, FTPClient> {
+    protected FTPClient doInBackground(Void... args) {
+        serverconnection ftpconnect =new serverconnection();
+        FTPClient ftp=ftpconnect.connectftp();
+        return ftp;
+    }
+
+    protected void onPostExecute(FTPClient result) {
+        Log.v("FTPTask", "FTP connection complete");
+        ftpClient = result;
+        //Where ftpClient is a instance variable in the main activity
+    }
+}
+*/
